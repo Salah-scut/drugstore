2477724f663ed6fc2bdf1b7dcfd95ba6c04e6910
Salah-scut <71164678+Salah-scut@users.noreply.github.com>
2021-11-23 15:43:57 +0800

    add create table employees

---
+++ sql/create tables.sql
@@ -0,0 +1,13 @@
+drop table employees cascade;
+create table employees
+(
+	employee_id varchar(9),
+	employee_name int not null,
+	job_id int default 3 not null ,
+	email varchar(30) null,
+	department_id int null,
+	password varchar(30) default '123' not null,
+	constraint table_name_pk
+		primary key (employee_id)
+);
+
