2299c921034ade1283033faddf4ab7981aa43f19
Salah-scut <71164678+Salah-scut@users.noreply.github.com>
2021-12-07 09:33:22 +0800

    add employee.class init_table.sql

---
+++ sql/init tables.sql
@@ -0,0 +1,7 @@
+INSERT INTO drugstore.employees (employee_id, employee_name, job_id, email, department_id, password)
+VALUES ('672328094', '嘉然', 2, 'bilibili.com/BV1FX4y1g7u8', 2, 'Diana0307'),
+       ('672353429', '贝拉', 2, 'bilibili.com/BV1oo4y1X7Ca', 1, 'Bella0717'),
+       ('672346917', '向晚', 3, 'bilibili.com/BV1oo4y1X7Ca', 2, 'Ava'),
+       ('351609538', '珈乐', 3, 'bilibili.com/BV1oo4y1X7Ca', 1, 'Carol1102'),
+       ('672342685', '乃琳', 3, 'bilibili.com/BV1oo4y1X7Ca', 1, 'huoguo'),
+       ('703007996', 'A-SOUL_Official', 1, 'bilibili.com/BV1vQ4y1Z7C2', null, DEFAULT);
